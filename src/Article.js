9ac73b01894377a0f895e9f3b75939b553533c52
Lorel <lorel.oboue@gmail.com>
2022-06-22 11:40:14 +0100

    inheritance added to article and book class

--- src/Article.js
+++ src/Article.js
@@ -1,22 +1,23 @@
-class Article {
-  constructor(title) {
-    this.title = title
-    this.onLoan = false
-  }
+const LibraryRental = require("./LibraryRental");
 
-  isOnLoan() {
-    return this.onLoan
-  }
-
-  checkIn() {
-     if (!this.isOnLoan()) throw new Error('item is not currently on loan')
-     this.onLoan = false
-  }
-
-  checkOut() {
-    if (this.isOnLoan()) throw new Error('item is currently on loan')
-    this.onLoan = true
-  }
+class Article extends LibraryRental {
+  // constructor(title) {
+  //   this.title = title;
+  //   this.onLoan = false;
+  // }
+  // isOnLoan() {
+  //   return this.onLoan;
+  // }
+  // checkIn() {
+  //   if (!this.isOnLoan()) throw new Error("item is not currently on loan");
+  //   this.onLoan = false;
+  // }
+  // checkOut() {
+  //   if (this.isOnLoan()) throw new Error("item is currently on loan");
+  //   this.onLoan = true;
+  // }
 }
 
-module.exports = Article
+module.exports = Article;
+
+// create a super class. take repetive behaviour seen in them and
